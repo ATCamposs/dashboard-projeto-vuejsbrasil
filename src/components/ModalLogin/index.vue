772123d6880ffae1a0148ec52d759820f29b7311
ATCamposs <andre.tatibano@gmail.com>
2021-03-24 17:05:43 -0300

    Termino criação modal e adicionado request e response interceptor

--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -47,12 +47,17 @@
 <script>
 import { reactive } from 'vue';
 import { useField } from 'vee-validate';
+import { useToast } from 'vue-toastification';
+import { useRouter } from 'vue-router';
 import useModal from '../../hooks/useModal';
 import { validateEmptyAndLength3, validateEmptyAndEmail } from '../../utils/validators';
+import services from '../../services';
 
 export default {
   setup() {
+    const router = useRouter();
     const modal = useModal();
+    const toast = useToast();
 
     const { value: emailValue, errorMessage: emailErrorMessage } = useField(
       'email',
@@ -76,7 +81,37 @@ export default {
       },
     });
 
-    function handleSubmit() {}
+    async function handleSubmit() {
+      try {
+        toast.clear();
+        state.isLoading = true;
+        const { data, errors } = await services.auth.login({
+          email: state.email.value,
+          password: state.password.value,
+        });
+
+        if (!errors) {
+          window.localStorage.setItem('token', data.token);
+          router.push({ name: 'Feedbacks' });
+          modal.close();
+          return;
+        }
+        if (errors.status === 404) {
+          toast.error('E-mail não encontrado');
+        }
+        if (errors.status === 401) {
+          toast.error('E-mail ou senha inválidos');
+        }
+        if (errors.status === 400) {
+          toast.error('Ocorreu um erro ao fazer o login');
+        }
+        state.isLoading = false;
+      } catch (error) {
+        state.isLoading = false;
+        state.hasErrors = !!error;
+        toast.error('Ocorreu um erro ao fazer o login');
+      }
+    }
 
     return { state, close: modal.close, handleSubmit };
   },
